9b3823ac6db30a2ca10dc477d006b18dbfa3d808
lthai423 <lthai423@gmail.com>
2016-08-29 14:56:44 -0700

    Added MongoDB ORM (Mongoose) onto package manager. Successfully connects to database local server instance.

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+// require node dependencies
 var express = require('express');
 var app = express();
 
+// require mongodb and ORM dependencies
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', (err) => console.log(err));
+db.once('open', () => console.log('MongoDB Connected'));
+
+// route handling
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+// turn server 'on'
 app.listen(3000, function () {
   console.log('Food2Me app listening on port 3000!');
 });
