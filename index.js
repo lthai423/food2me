d2cbc7d2df7c1c684fc8b5a1136eab6b5a6b2a2e
lthai423 <lthai423@gmail.com>
2016-08-29 21:49:42 -0700

    Successfully posts.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // require node dependencies
 var express = require('express');
 var app = express();
+module.exports = app;
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded()); 
+
 
 // require file stream
 var fs = require('fs');
 
 var router = require('./routes.js');
+// router.init(app);
+
+
 
 app.set('port', process.env.PORT || 5000);
 
@@ -22,10 +30,14 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-// route all to router
+// serve static home page. 
+// app.use(express.static('/'));
+
+// route all others
 app.get('*', router);
 
 // turn server 'on'
-app.listen(process.env.PORT, function () {
+app.listen(5000, function (err) {
+	if (err) console.log(err)
   console.log('Food2Me app listening on port 5000!');
 });
--- routes.js
+++ routes.js
@@ -1,16 +1,24 @@
-var express = require('express');
-var router = express.Router();
+var router = require('./index.js');
+var fs = require('fs');
 
-// index
 router.get('/', function(req, res){
-	res.render('index', {});
+	res.statusCode = 200;
+	fs.readFile('./index.html', function(err, file){
+		if(!err) {
+			res.setHeader("Content-Type", "text/html");
+			res.send(file);
+		}
+	})
 });
 
-router.get('/hello', function(req, res){
-	res.send("hello");
+// signup
+router.post('/signup', function(req, res){
+	console.log(req.body);
+	res.statusCode = 202;
+	res.send();
 });
 
-// 404
+// catch 404's
 router.get('/*', function(req, res){
 	res.statusCode = 404;
 	res.send("404. NOT FOUND");
