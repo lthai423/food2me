4702f1c8d86cc5f80e8d2c6e5b5e1d4cf6adbecf
lthai423 <lthai423@gmail.com>
2016-08-29 19:26:11 -0700

    Added react dependency.

--- routes.js
+++ routes.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+// index
 router.get('/', function(req, res){
 	res.render('index', {});
+});
+
+router.get('/hello', function(req, res){
+	res.send("hello");
+});
+
+// 404
+router.get('/*', function(req, res){
+	res.statusCode = 404;
+	res.send("404. NOT FOUND");
 });
 
 module.exports = router;
