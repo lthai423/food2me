25e204ad2bce0402e484f43b374604419cc9c037
lthai423 <lthai423@gmail.com>
2016-08-29 19:28:36 -0700

    Mongo configure for deployment.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var router = require('./routes.js');
 
 // require mongodb and ORM dependencies
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://tranquil-hollows-38103/herokuapp/test');
 var db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 db.once('open', () => console.log('MongoDB Connected'));
